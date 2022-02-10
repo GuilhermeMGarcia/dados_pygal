49112c4f13a5f1b5e68b40382be7b0ca594a90a0
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-02-10 04:05:09 -0300

    salvando_plt

--- scatter_squares.py
+++ scatter_squares.py
@@ -16,4 +16,4 @@ plt.axis([0, 1100, 0, 1100000])
 # Define o tamanho dos rotulos das marcaçoes
 plt.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
