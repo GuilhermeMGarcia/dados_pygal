96fb6e05a9b016c4d81ffdf9621ca2986d4d8129
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-02-15 09:32:50 -0300

    limpando_os_eixos

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Cria um passeio aleatorio e plota os pontos
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+    # Cria um passeio aleatorio e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=15)
+
+    # Enfatiza o primeiro e o ultimo ponto
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+                s=100)
+
+    # Remove os eixos
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_runing = input("Make another walk? (y/n): ")
+    if keep_runing == 'n':
+        break
