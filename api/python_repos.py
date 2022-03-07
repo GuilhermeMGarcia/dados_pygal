97dfe28285bd7d07bef529d914d239f4fdf275e8
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-03-07 15:24:56 -0300

    visualizando_repo_no_pygal

--- api/python_repos.py
+++ api/python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 
 # Faz uma chamada de API e armazena a resposta
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,14 +14,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explora informaçoes sobre os repositorios
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Analiza o primeiro repositorio
-repo_dict = repo_dicts[0]
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('Name:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Cria a visualizaçao
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
