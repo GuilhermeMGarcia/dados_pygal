cea4bffaad907501790fcd72d06afff1403d22ea
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-03-07 17:32:38 -0300

    link_in_bar

--- api/python_repos.py
+++ api/python_repos.py
@@ -15,10 +15,16 @@ print("Total repositories:", response_dict['total_count'])
 # Explora informaçoes sobre os repositorios
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dics = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dic = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dics.append(plot_dic)
 
 # Cria a visualizaçao
 my_style = LS('#333366', base_style=LCS)
@@ -37,5 +43,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dics)
 chart.render_to_file('python_repos.svg')
